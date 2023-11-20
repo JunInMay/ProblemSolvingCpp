5646567f0d8297a06cd1f9a3a43f09b0168992d4
Chana <twszak01@gmail.com>
2023-11-20 23:27:36 +0900

    2023-11-20, BOJ 2587

---
+++ BOJ/BOJ/BOJ2587.cpp
@@ -0,0 +1,46 @@
+// https://www.acmicpc.net/problem/2587 boj 2587 ´ëÇ¥°ª2 2023-11-20
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void bubble_sort(vector<int>& list)
+{
+	int length = list.size();
+
+	for (int i = 0; i < length - 1; i++)
+	{
+		for (int j = i + 1; j < length; j++)
+		{
+			if (list[i] > list[j])
+			{
+				int temp = list[i];
+				list[i] = list[j];
+				list[j] = temp;
+			}
+		}
+	}
+}
+
+int main()
+{
+	vector<int> list;
+
+	for (int i = 0; i < 5; i++)
+	{
+		int input;
+		cin >> input;
+		list.push_back(input);
+	}
+
+	bubble_sort(list);
+
+	int sum = 0;
+	for (int i = 0; i < 5; i++)
+	{
+		sum += list[i];
+	}
+	int mid = list[list.size() / 2];
+	int avg = sum / 5;
+	cout << avg << "\n" << mid;
+}
