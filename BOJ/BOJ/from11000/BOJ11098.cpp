eb4f08c821ef6db0e5c3c91b3d02ff98094e7fda
Chana <twszak01@gmail.com>
2023-12-24 19:26:23 +0900

    2023-12-24, BOJ11098

---
+++ BOJ/BOJ/from11000/BOJ11098.cpp
@@ -0,0 +1,28 @@
+// https://www.acmicpc.net/problem/11098 BOJ 11098  √øΩ√∏¶ µµøÕ¡‡! 2023-12-24
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int case_count;
+	cin >> case_count;
+
+	while (case_count--) {
+		int player_count;
+		cin >> player_count;
+
+		int max_price = -1;
+		string max_player = "";
+		for (int i = 0; i < player_count; i++) {
+			string name;
+			int price;
+			cin >> price >> name;
+
+			if (price > max_price) {
+				max_price = price;
+				max_player = name;
+			}
+		}
+		cout << max_player << '\n';
+	}
+}
