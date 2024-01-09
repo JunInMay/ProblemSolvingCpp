f5e44cde2afdec68783470bb355a2a25e361c6cf
Chana <twszak01@gmail.com>
2024-01-09 22:24:05 +0900

    2024-01-09, BOJ 27325

---
+++ BOJ/BOJ/from27300/BOJ27325.cpp
@@ -0,0 +1,27 @@
+// https://www.acmicpc.net/problem/27325 BOJ 27325 3 ªÄªÎßÕ (Three Boxes) 2024-01-08
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	int l;
+	string s;
+	cin >> l >> s;
+
+	int box = 1;
+	int result = 0;
+	for (int i = 0; i < l; i++) {
+		int target = 0;
+		if (s[i] == 'L') {
+			target = max(box - 1, 1);
+		}
+		else {
+			target = min(box + 1, 3);
+		}
+		if (target == 3) ++result;
+		box = target;
+	}
+
+	cout << result;
+}
