df1df4535f0a50e24c8493d9fbae7cffe2573523
Chana <twszak01@gmail.com>
2024-01-10 17:21:12 +0900

    2024-01-10, BOJ24294

---
+++ BOJ/BOJ/from24200/BOJ24294.cpp
@@ -0,0 +1,13 @@
+// https://www.acmicpc.net/problem/24294 BOJ 24294 ¬¤¬²¬¡¬¥¬ª¬¯¬¡ 2024-01-10
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	int w1, h1, w2, h2;
+
+	cin >> w1 >> h1 >> w2 >> h2;
+
+	cout << h1 * 2 + h2 * 2 + w1 + w2 + abs(w1 - w2) + 4;
+}
