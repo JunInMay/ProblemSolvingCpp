42c047b00ce993850112d9103f9ada76e19e4c43
Joonyi <twszak01@gmail.com>
2024-01-30 20:44:19 +0900

    2024-01-30, BOJ31281

---
+++ BOJ/BOJ/from31200/BOJ31281.cpp
@@ -0,0 +1,16 @@
+// https://www.acmicpc.net/problem/31281 BOJ 31281 ¬©¬­¬¡¬´¬¯¬¡¬´¬¡ ¬³¬²¬¦¬¥¬¡ 2024-01-30
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	long long arr[3];
+	for (int i = 0; i < 3; i++) {
+		cin >> arr[i];
+	}
+
+	sort(&arr[0], &arr[3]);
+
+	cout << arr[1];
+}
