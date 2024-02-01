6b23cccc73de88895f9fc1cbc8c75ef4b568dfba
Joonyi <twszak01@gmail.com>
2024-02-01 12:52:46 +0900

    2024-02-01, BOJ2504

---
+++ BOJ/BOJ/from2500/BOJ2504.cpp
@@ -0,0 +1,75 @@
+// https://www.acmicpc.net/problem/2504 BOJ 2504 °ýÈ£ÀÇ °ª 2024-02-01
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int idx;
+int inputSize;
+string input;
+int error = -10000;
+
+int dfs() {
+	char open = 0;
+	int value = 0;
+	int result = 0;
+	while (true) {
+		if (idx >= inputSize) return error;
+		char now = input[idx];
+		if (open == 0) {
+			if (now == ')' || now == ']') {
+				idx++;
+				return error;
+			}
+			open = input[idx++];
+			continue;
+		}
+
+		// openÀÌ ÀÖ´Âµ¥ ´Ù½Ã ¿­·ÈÀ» °æ¿ì´Â »õ·Î °ýÈ£°¡ »ý±ä °Í
+		if (now == '(' || now == '[') {
+			value += dfs();
+			continue;
+		}
+		// °ýÈ£°¡ Àß¸ø ´ÝÇûÀ» °æ¿ì ¿¡·¯
+		if ((open == '(' && now == ']') || (open == '[' && now == ')'))
+			return error;
+
+		if (!value) value = 1;
+		// °ýÈ£°¡ ´ÝÇûÀ» °æ¿ì ¹ë·ù °è»ê
+		if (now == ')') {
+			result += value * 2;
+			idx++;
+			open = 0;
+			value = 0;
+			break;
+		}
+		if (now == ']') {
+			result += value * 3;
+			idx++;
+			open = 0;
+			value = 0;
+			break;
+		}
+	}
+	return result;
+}
+
+int main() {
+	idx = 0;
+	cin >> input;
+	inputSize = input.size();
+
+	int result = 0;
+	while (idx < inputSize) {
+		result += dfs();
+	}
+	result = max(result, 0);
+	cout << result << '\n';
+	
+}
+/*
+https://www.acmicpc.net/board/view/42873
+¹Ý·Ê
+([)
+
+*/
