98067565b0212900e6ce122e1ffc3d707c54b677
Chana <twszak01@gmail.com>
2024-02-11 19:13:24 +0900

    2024-02-11,BOJ 26714

---
+++ BOJ/BOJ/from26700/BOJ26714.cpp
@@ -0,0 +1,27 @@
+﻿// https://www.acmicpc.net/problem/26714 BOJ 26714 Liczenie punktów 2024-02-11
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N;
+	string input;
+	cin >> N >> input;
+
+	int idx = 0;
+	int result = 0;
+	for (int i = 0; i < 10; i++) {
+		bool correct = true;
+		for (int j = 0; j < N / 10; j++) {
+			idx = (i * N / 10) + j;
+			if (input[idx] == 'N') {
+				correct = false;
+				break;
+			}
+		}
+
+		if (correct) result++;
+	}
+
+	cout << result;
+}
