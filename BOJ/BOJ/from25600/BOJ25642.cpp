f2129611af3b05e0ba34701e70fddf4b3ba26d22
Chana <twszak01@gmail.com>
2024-04-16 22:12:26 +0900

    2024-04-16, BOJ25642

---
+++ BOJ/BOJ/from25600/BOJ25642.cpp
@@ -0,0 +1,19 @@
+// https://www.acmicpc.net/problem/25642 BOJ 25642 ¡£∞°∂Ù ∞‘¿” 2024-04-16
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+
+	string result = "yt";
+	if (a + b >= 5) {
+		result = "yt";
+	}
+	else if (a + b + a >= 5) {
+		result = "yj";
+	}
+
+	cout << result;
+}
