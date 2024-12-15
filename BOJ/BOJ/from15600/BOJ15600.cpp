634df0314e8b5297dd14bab8fa55bdc3f49ad6c2
Chana <twszak01@gmail.com>
2024-12-15 23:14:40 +0900

    [2024-12-15] BOJ 15600

---
+++ BOJ/BOJ/from15600/BOJ15600.cpp
@@ -0,0 +1,12 @@
+// https://www.acmicpc.net/problem/15600 2024-12-15 BOJ 15600 Boss Battle
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  cout << max(1, n - 2);
+}
